5bf534ec9066e468ddbd6fc8ea288f33145865f6
Yasser BDJ (Ro0t-96) <by.root96@gmail.com>
2021-06-04 16:47:28 +0100

    1.1.4

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup,find_packages
 setup(
-    name="hiphp",
-    version="0.1.7",
+    name="ashar",
+    version="1.1.4",
     author="Yasser BDJ (Ro0t96)",
     author_email="by.root96@gmail.com",
-    description='''A package for controlling a php-based website.''',
+    description='''This project is for data encryption with password protection.''',
     long_description_content_type="text/markdown",
     long_description=open('README.md','r').read(),
     license="Apache Software License",
     packages=find_packages(),
-    url="https://github.com/byRo0t96/hiphp",
+    url="https://github.com/byRo0t96/ashar",
     project_urls={
         'Author WebSite': "https://byro0t96.github.io/",
     },
-    install_requires=['pipincluder', 'requests', 'ashar', 'hexor'],
-    keywords=['python', 'hiphp', 'php', 'cli'],
+    install_requires=['pipincluder'],
+    keywords=['python', 'ashar', 'encode', 'decode', 'key', 'password', 'encrypt anything with password'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
